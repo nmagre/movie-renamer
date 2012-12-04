dad3b81c8036fe3ff55e672bc6b1474dd4877a37
Magré Nicolas <contact@movie-renamer.fr>
2012-12-04 05:36:00 +0100

    Fixed post install script (windows)

--- mr-ui/Izpack/Windows/postinst.bat
+++ mr-ui/Izpack/Windows/postinst.bat
@@ -1,9 +1,12 @@
-@echo on
+@echo off
 SET pp=%1
-SET hdd=%pp:~0,3%
-SET pathInst=%pp:~3%
-cd %hdd%
 
+SET hdd=%pp:~0,2%
+SET pathInst=%pp:~2%
+
+
+%hdd%
 cd %pathInst%
-IF EXIST %3 DEL %3 
+
+IF EXIST %3 DEL %3
 rename %2 %3
