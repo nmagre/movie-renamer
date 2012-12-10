2e471740504b2d0f39d474c122e113959ff3a397
QUÉMÉNEUR Simon <simon.quemeneur@gmail.com>
2012-12-10 10:00:38 +0100

    Add tests for Cache

---
+++ mr-core/test/fr/free/movierenamer/utils/CacheTest.java
@@ -0,0 +1,37 @@
+/*
+ * mr-core
+ * Copyright (C) 2012 Nicolas Magré
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.free.movierenamer.utils;
+
+import org.junit.Test;
+
+/**
+ * Class CacheTest
+ * @author Simon QUÉMÉNEUR
+ */
+public class CacheTest {
+  @Test
+  public void testMultiThread() {
+    //TODO test the concurrent access
+    new Thread() {
+      @Override
+      public void run() {
+        Cache.clearAllCache();
+      }
+    }.start();
+  }
+}
